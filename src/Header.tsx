a049670dab0e7f35f662cc2002c88262fc0c10c9
Nastassia27 <anastasia.derugo@gmail.com>
2023-02-26 15:49:25 +0300

    create header

---
+++ src/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <div>
+            <a href="" className="href">Home</a>
+            <a href="" className="href">News</a>
+            <a href="" className="href">Messages</a>
+        </div>
+    )
+}
+
+ export default Header;
