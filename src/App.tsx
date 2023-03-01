ff36a4bede3d28be7f628d423a4a4043857a1f65
Nastassia27 <anastasia.derugo@gmail.com>
2023-03-01 22:45:31 +0300

    create Profile component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 
 function App() {
@@ -9,28 +10,7 @@ function App() {
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
-            <div className='content'>
-                <div>
-                    <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'/>
-                </div>
-                <div>
-                    ava+dexcription
-                </div>
-                <div>
-                    my posts
-                    <div>
-                        new post
-                    </div>
-                    <div>
-                        <div>
-                            post1
-                        </div>
-                        <div>
-                            post2
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Profile/>
         </div>
 
     );
