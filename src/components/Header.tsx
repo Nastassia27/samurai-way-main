08e9801458daf62b5bc9cfec313f242d16225f13
Nastassia27 <anastasia.derugo@gmail.com>
2023-03-01 22:35:31 +0300

    create header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <header className='header'>
+            <img src='https://png.pngtree.com/png-clipart/20201208/original/pngtree-red-and-black-logo-png-image_5517319.jpg' />
+
+        </header>
+    )
+}
+
+export default Header;
