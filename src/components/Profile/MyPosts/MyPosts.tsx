e0ea3b675a5c6d591ac49802c679ab5ca3438bfa
Nastassia27 <anastasia.derugo@gmail.com>
2023-03-18 15:41:03 +0300

    posts array and map method

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,17 +3,29 @@ import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+
+
+    let posts = [
+        {id: 1, message: 'hi', likesCount: 12},
+        {id: 2, message: 'How are you', likesCount: 10},
+        {id: 3, message: 'Good', likesCount: 100}
+    ]
+    let postsElements = posts.map((p) => <Post message={p.message} likesCount={p.likesCount}/>)
     return (
-        <div>
-            <textarea></textarea>
-            <button>Add post</button>
+        <div className={s.postsBlock}>
+            <h2>My Posts</h2>
+            <div>
+                <textarea></textarea></div>
+            <div>
+                <button>Add post</button>
+            </div>
 
             <div>
                 new post
             </div>
             <div className={s.posts}>
-               <Post message='Hi, how are you?' likesCount='0'/>
-               <Post message="It's my first post, how are you?" likesCount='23'/>
+
+                {postsElements}
 
             </div>
         </div>
