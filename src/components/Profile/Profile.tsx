d1e13496e0bf3bf3c139a84fe65e2adb4cef9632
Nastassia27 <anastasia.derugo@gmail.com>
2023-03-18 15:42:12 +0300

    refactor profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import s from './Profile.module.css'
 import MyPosts from "./MyPosts/MyPosts";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
-    return(
-        <div className={s.content}>
-            <div>
-                <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'/>
-            </div>
-            <div>
-                ava+description
-            </div>
-            <MyPosts />
+    return (
+        <div>
+            <ProfileInfo/>
+            <MyPosts/>
         </div>
     )
 }
