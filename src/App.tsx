a9ef60f0e63bfcdb5ad8686e77effaa39a486967
Nastassia27 <anastasia.derugo@gmail.com>
2023-04-28 12:30:08 +0300

    typization fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,15 @@ import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route} from "react-router-dom";
+import {RootStateType} from "./redux/state";
+
+type AppPropsType={
+    state: RootStateType
+}
+
+const App: React.FC<AppPropsType> = (props) =>{
 
 
-function App() {
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
@@ -15,8 +21,10 @@ function App() {
                 <Navbar/>
 
                 <div className='app-wrapper-content'>
-                    <Route path='/dialogs' component={Dialogs}/>
-                    <Route path='/profile' component={Profile}/>
+
+
+                    <Route path='/dialogs' render={() => <Dialogs state={props.state.dialogsPage}/>}/>
+                    <Route path='/profile' render={() => <Profile state={props.state.profilePage}/>}/>
                 </div>
             </div>
         </BrowserRouter>
