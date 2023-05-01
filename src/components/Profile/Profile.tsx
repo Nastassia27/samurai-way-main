bfc3e33ed9c73ad28cd5ddfb3546495d832ee01d
Nastassia27 <anastasia.derugo@gmail.com>
2023-05-01 20:55:11 +0300

    type profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import s from './Profile.module.css'
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
+import {ProfilePageType} from "../../redux/state";
 
-const Profile = () => {
+
+type ProfileType = {
+    state: ProfilePageType
+}
+const Profile: React.FC<ProfileType> = (props) => {
+
+
+
     return (
         <div>
             <ProfileInfo/>
-            <MyPosts/>
+            <MyPosts posts={props.state.posts} />
         </div>
     )
 }
