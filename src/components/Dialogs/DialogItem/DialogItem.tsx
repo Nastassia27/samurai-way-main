baf39ba2dd9aaa60e92af6f905a91844adae3f7a
Nastassia27 <anastasia.derugo@gmail.com>
2023-05-01 20:54:05 +0300

    type dialogItem

--- src/components/Dialogs/DialogItem/DialogItem.tsx
+++ src/components/Dialogs/DialogItem/DialogItem.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import s from './../Dialogs.module.css'
 import {NavLink} from "react-router-dom";
+import {DialogType} from "../../../redux/state";
 
-const DialogItem = (props: any) => {
-    let path = '/dialogs/1' + props.id
-    return (<div className={s.dialog + ' ' + s.active}>
-            <NavLink to={path}> {props.name}</NavLink>
+type PropsType={
+    dialog: DialogType
+}
+
+const DialogItem: React.FC<PropsType>  = (props) => {
+    let path = '/dialogs/1' + props.dialog.id
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <img src='https://img.championat.com/s/735x490/news/big/y/g/avatar-2-sobral-v-rossii-bolshe-2-4-mlrd-rublej_16758793371084217002.jpg'/>
+            <NavLink to={path}> {props.dialog.name}</NavLink>
         </div>
 
     )
