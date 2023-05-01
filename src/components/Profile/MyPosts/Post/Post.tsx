aaa2411a04ecfb5344325ef0eeb2c2da4a441b77
Nastassia27 <anastasia.derugo@gmail.com>
2023-05-01 20:54:58 +0300

    type post

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import s from './Post.module.css'
+import {PostType} from "../../../../redux/state";
 
-const Post = (props: any) => {
+
+type PropsType={
+    post: PostType
+}
+const Post: React.FC<PropsType> = (props) => {
     return (
         <div>
 
                 <div className={s.item}>
                     <img src='https://img.championat.com/s/735x490/news/big/y/g/avatar-2-sobral-v-rossii-bolshe-2-4-mlrd-rublej_16758793371084217002.jpg'/>
-                    {props.message}
+                    {props.post.message}
                     <div>
-                    <span>like</span> {props.likesCount}
+                    <span>like</span> {props.post.likesCount}
                     </div>
                 </div>
 
