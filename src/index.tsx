fa156789ad6593306ba1e0899ddc97bd9b1b3523
Nastassia27 <anastasia.derugo@gmail.com>
2023-05-03 18:45:29 +0300

    short rerender with store

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {rerenderEntireTree} from "./render";
+import store from "./redux/state";
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+/*
+const rerenderEntireTree=(state)=>{
+    ReactDOM.render(
+        <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>,
+
+
+        document.getElementById('root')
+    );
+*/
+//}
+
+
+store.subscribe(rerenderEntireTree)
+rerenderEntireTree();
+//addPost('Samurai JS')
