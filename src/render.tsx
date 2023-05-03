0195b92b2f3bbc893089ea406a3d095175498f40
Nastassia27 <anastasia.derugo@gmail.com>
2023-05-03 20:10:47 +0300

    dispatch render

--- src/render.tsx
+++ src/render.tsx
@@ -7,7 +7,7 @@ import store from "./redux/state"
 
 export const rerenderEntireTree=()=>{
     ReactDOM.render(
-        <App state={store.getState()} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}/>,
+        <App state={store.getState()} dispatch={store.dispatch.bind(store)}/>,
 
 
         document.getElementById('root')
