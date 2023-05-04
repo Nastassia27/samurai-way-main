64355813a92b2e33e0c1b269b4c9729e57b09be4
Nastassia27 <anastasia.derugo@gmail.com>
2023-05-04 13:53:55 +0300

    change dialog reducer

--- src/redux/dialogs-reducer.tsx
+++ src/redux/dialogs-reducer.tsx
@@ -1,5 +1,34 @@
-const dialogsReducer = (state, action)=>{
+import {ActionsTypes, MessageType, RootStateType} from "./state";
+
+const ADD_MESSAGE = 'ADD-MESSAGE';
+const UPDATE_NEW_MESSAGE = 'UPDATE-NEW-MESSAGE';
+
+const dialogsReducer = (state: any, action: ActionsTypes) => {
+
+    switch (action.type) {
+        case ADD_MESSAGE:
+            const newMessage: MessageType = {
+                id: 5,
+                message: state.newMessageBody,
+            };
+            state.messages.push(newMessage)
+            state.newMessageBody = ''
+            return state;
+        case UPDATE_NEW_MESSAGE:
+            state.newMessageBody = action.newMess
+            return state;
+        default:
+            return state;
+    }
 
 
-    return state;
 }
+
+export const addMessageActionCreator =()=>({type: ADD_MESSAGE}) as const
+
+export const updateNewMessageAC = (message: string)=> ({
+    type: UPDATE_NEW_MESSAGE,
+    newMess: message
+}) as const
+
+export default dialogsReducer;
