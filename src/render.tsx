1cda51d2c3125a22e312ffd0d248fe9a8b58df33
Nastassia27 <anastasia.derugo@gmail.com>
2023-05-04 12:02:10 +0300

    browser router app

--- src/render.tsx
+++ src/render.tsx
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 //import state, {addPost, RootStateType, updateNewPostText} from "./redux/state";
 import store from "./redux/state"
+import {BrowserRouter} from "react-router-dom";
 
 export const rerenderEntireTree=()=>{
     ReactDOM.render(
+        <BrowserRouter>
         <App state={store.getState()} dispatch={store.dispatch.bind(store)}/>,
 
-
+        </BrowserRouter>,
         document.getElementById('root')
     );
 }
