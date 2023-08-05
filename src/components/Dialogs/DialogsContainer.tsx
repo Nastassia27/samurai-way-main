696f5b02f06f2240ca9370648aa4b011298738d6
Nastassia27 <anastasia.derugo@gmail.com>
2023-08-05 15:18:13 +0300

    use connect() for Dialogs

--- src/components/Dialogs/DialogsContainer.tsx
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -1,8 +1,5 @@
 
 import React, {ChangeEvent} from "react";
-import s from './Dialogs.module.css'
-import Message from "./Message/Message";
-import DialogItem from "./DialogItem/DialogItem";
 import store, {
     ActionsTypes,
 } from "../../redux/redux-store";
@@ -10,31 +7,30 @@ import store, {
 import {addMessageActionCreator,updateNewMessageAC} from "../../redux/dialogs-reducer";
 import {AppStateType} from "../../redux/redux-store";
 import Dialogs from "./Dialogs";
+import {connect} from "react-redux";
 
 
-type DialogsType={
-    store: AppStateType
-    //state: DialogPageType
-    dispatch:(action: ActionsTypes)=>void
-
-}
-const DialogsContainer: React.FC<DialogsType> = (props) => {
-    let state = props.store.dialogsPage
-
-    const addMessage = ()=>{
-        props.dispatch(addMessageActionCreator())
+/*
+let state = props.store.dialogsPage
+*/
+//первый объект для возвращения данные из стэйта а второй callbacks
+let mapStateToProps = (state: AppStateType)=>{
+    return {
+        dialogsPage:state.dialogsPage
     }
-    const newMessageChangeHandler=(body: string)=>{
-        props.dispatch(updateNewMessageAC(body))
-    }
-
-    return (
-        <Dialogs updateNewMessageBody={newMessageChangeHandler}
-                 addMessage={addMessage}
-                 dialogsPage={state}
-
-        />
-    )
 }
+
+let mapDispatchToProps = (dispatch: (action: ActionsTypes)=>void)=>{
+    return {
+        updateNewMessageBody:(body: string)=>{
+            dispatch(updateNewMessageAC(body))
+        },
+        addMessage:()=>{
+            dispatch(addMessageActionCreator())
+        }
+    }
+}
+
+const DialogsContainer = connect(mapStateToProps,mapDispatchToProps)(Dialogs);
 
 export default DialogsContainer
