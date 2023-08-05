b5c3d4908b8eb7f75d458fc51f775dddec3694ae
Nastassia27 <anastasia.derugo@gmail.com>
2023-08-05 16:42:40 +0300

    delete subscriber

--- src/render.tsx
+++ src/render.tsx
@@ -7,22 +7,21 @@ import store, {AppStateType} from "./redux/redux-store"
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
-export const rerenderEntireTree=(state: AppStateType)=>{
 
-    ReactDOM.render(
-        <BrowserRouter>
-            <Provider store={store}>
-        <App  />,
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App/>,
             {/*state={state}*/}
-{/*
+            {/*
                 dispatch={store.dispatch.bind(store)} store = {store}
 */}
-            </Provider>
-        </BrowserRouter>,
-        document.getElementById('root')
-    );
-}
-rerenderEntireTree(store.getState())
+        </Provider>
+    </BrowserRouter>,
+    document.getElementById('root'))
+
+
+//rerenderEntireTree(store.getState())
 
 
 //addPost('Samurai JS')
