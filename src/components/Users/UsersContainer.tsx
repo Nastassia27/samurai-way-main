8b5bdbc438d508fe453525a6e72a0803f5ab7116
Nastassia27 <anastasia.derugo@gmail.com>
2023-08-07 14:36:49 +0300

    Complete users page

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Users from "./Users";
+import {connect} from "react-redux";
+import {ActionsTypes, AppStateType} from "../../redux/redux-store";
+import {followAC, setUsersAC, unfollowAC, UsersType} from "../../redux/users-reducer";
 
-const UsersContainer =()=>{
-    return
+const mapStateToProps = (state: AppStateType) => {
+    return {
+        users: state.usersPage.users
+    }
 }
+
+const mapDispatchToProps = (dispatch: (action: ActionsTypes) => void) => {
+    return {
+        follow: (userId: number) => {
+            dispatch(followAC(userId))
+        },
+        unfollow: (userId: number) => {
+            dispatch(unfollowAC(userId))
+        },
+        setUsers: (users: Array<UsersType>) => {
+            dispatch(setUsersAC(users))
+        },
+    }
+}
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
 
 export default UsersContainer
